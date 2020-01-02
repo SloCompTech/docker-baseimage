90c3457db61be36dd61662df17150eb9fb7559ba
Martin <martin.dagarin@gmail.com>
2020-01-02 17:41:04 +0100

    Fixed indentation, moved failmode to 15, so additional scripts can be put inbetween

--- root/usr/local/etc/bi/generic-finish.sh
+++ root/usr/local/etc/bi/generic-finish.sh
@@ -20,9 +20,9 @@ if [ -n "$service_name" ] && [ "$FAIL_MODE" == "count" ] && [ -n "$FAIL_MODE_COU
   count=0
   if [ -f "/tmp/service_count_$service_name" ]; then
     count=$(cat /tmp/service_count_$service_name)
-	fi
-	((count++))
-	if [ $count -gt $FAIL_MODE_COUNT ]; then
+  fi
+  ((count++))
+  if [ $count -gt $FAIL_MODE_COUNT ]; then
     rm /tmp/service_count_$service_name
     exec s6-svscanctl -t /var/run/s6/services
   else
